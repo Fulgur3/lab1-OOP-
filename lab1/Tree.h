d23f14d3eae97d587c4a37319a47728d31e20f7f
Fulgur3 <74140068+Fulgur3@users.noreply.github.com>
2021-09-27 20:48:05 +0300

    Update Tree.h

--- lab1/Tree.h
+++ lab1/Tree.h
@@ -3,8 +3,7 @@
 #include <string>
 #include <vector>
 using namespace std;
-
-//Tree
+#define COUNT 10
 
 class Tree {
 private:
@@ -33,4 +32,89 @@ public:
 	void printNode(Node* current);
 	void deleteTree(Node* current);
 
+};
+
+
+//===========================================================================
+
+
+
+class BinaryTree {
+private:
+    class Node {
+    public:
+        int id;
+        int year;
+        int month;
+        int day;
+        int hour;
+        int minute;
+        int second;
+        Node* left;
+        Node* right;
+        Node(int year, int month, int day, int hour, int minute, int second);
+    };
+    Node* root;
+    int count;
+public:
+    BinaryTree(int year, int month, int day, int hour, int minute, int second);
+    ~BinaryTree();
+    Node* getRoot();
+    void setId(Node* current);
+    void append(int year, int month, int day, int hour, int minute, int second);
+    Node* deleteNode();
+    void deleteTree();
+    void output2dTreeUtil(Node* current, int space);
+    void output2dTree(Node* current);
+    void outTree();
+    void printNode(Node* current);
+    void print2DUtil(Node* current, int space);
+    bool checkValues(Node* current, int year, int month, int day, int hour, int minute, int second);
+    bool checkEqual(Node* current, int year, int month, int day, int hour, int minute, int second);
+    bool check2Nodes(Node* current, Node* newNode);
+    bool checkEqual(Node* current, Node* newNode);
+    Node* getParent(Node* current, Node* tree);
+    //Not completed
+    void deleteNodeByValue(int year, int month, int day, int hour, int minute, int second);
+    void deleteNodeByIndex(int index);
+    void searchNodeByValue(int year, int month, int day, int hour, int minute, int second);
+    void preOrder();
+
+};
+
+//===========================================================================
+
+class BinarySearchTree {
+private:
+    class Node {
+    public:
+        int id;
+        int year;
+        int month;
+        int day;
+        int hour;
+        int minute;
+        int second;
+        Node* left;
+        Node* right;
+        Node(int year, int month, int day, int hour, int minute, int second);
+    };
+    Node* root;
+    int count;
+public:
+    BinarySearchTree(int year, int month, int day, int hour, int minute, int second);
+    ~BinarySearchTree();
+    void setId(Node* current);
+    bool checkValues(Node* current, int year, int month, int day, int hour, int minute, int second);
+    bool checkEqual(Node* current, int year, int month, int day, int hour, int minute, int second);
+    bool check2Nodes(Node* current, Node* newNode);
+    bool checkEqual(Node* current, Node* newNode);
+    void append(int year, int month, int day, int hour, int minute, int second);
+    void output2dTreeUtil(Node* current, int space);
+    void output2dTree(Node* current);
+    void print2DUtil(Node* current, int space);
+    void outTree();
+    void printNode(Node* current);
+    Node* deleteNode();
+    void deleteTree();
 };
